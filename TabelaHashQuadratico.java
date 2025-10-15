1c223a70e9ceb5d2a0d231ad20f32f719ac917f8
Lucas <cuissesdepaula@hotmail.com>
2025-10-14 21:30:45 -0300

    atualização da TabelaHashQuadratico.java
    correção de impressão de espaçamentos na tabela hash quadratico

--- TabelaHashQuadratico.java
+++ TabelaHashQuadratico.java
@@ -192,8 +192,8 @@ public class TabelaHashQuadratico {
         float media = (float)somaEspacos/contagemEspacos;
 
         System.out.println("Calculando espaçamentos/gap:");
-        System.out.println("Maior espaçamento da tabela hash encadeada: " + espacoMaior);
-        System.out.println("Menor espaçamento da tabela hash encadeada: " + espacoMenor);
+        System.out.println("Maior espaçamento da tabela hash quadratico: " + espacoMaior);
+        System.out.println("Menor espaçamento da tabela hash quadratico: " + espacoMenor);
         System.out.println("Media dos espaçamentos encontrados: " + media);
         System.out.println();
 
