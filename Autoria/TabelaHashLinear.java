b6f64461c68a69f9f57f91bc2283c5407f3ee1c3
Lucas <cuissesdepaula@hotmail.com>
2025-10-14 21:36:13 -0300

    atualização da TabelaHashLinear autoria
    correção de impressão de gaps

--- Autoria/TabelaHashLinear.java
+++ Autoria/TabelaHashLinear.java
@@ -173,8 +173,8 @@ public class TabelaHashLinear {
         float media = (float)somaEspacos/contagemEspacos;
 
         System.out.println("Calculando espaçamentos/gap:");
-        System.out.println("Maior espaçamento da tabela hash encadeada: " + espacoMaior);
-        System.out.println("Menor espaçamento da tabela hash encadeada: " + espacoMenor);
+        System.out.println("Maior espaçamento da tabela hash linear: " + espacoMaior);
+        System.out.println("Menor espaçamento da tabela hash linear: " + espacoMenor);
         System.out.println("Media dos espaçamentos encontrados: " + media);
         System.out.println();
 
